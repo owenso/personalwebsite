219837e26d20cb731889e4fd69887d8481b85e2f
Owens O'Brien <owensobrien@gmail.com>
2015-11-04 09:43:41 -0500

    about me

--- js/app.js
+++ js/app.js
@@ -15,5 +15,5 @@ var expandEmail = function(){
 };
 
 var expandGithub = function(){
-		$('#clickresult').hide().html('<a href="https://github.com/owenso" target="_blank">https://github.com/owenso</a>').fadeIn(400);
+		$('#clickresult').hide().html('<p><a href="https://github.com/owenso" target="_blank">https://github.com/owenso</a></p>').fadeIn(400);
 }
